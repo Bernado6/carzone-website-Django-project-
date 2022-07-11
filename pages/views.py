32031b89ae9a6019becedb1bb4ffada7c47c45ae
Bernardo6 <bernardo98@students.uonbi.ac,ke>
2022-07-11 22:47:16 +0300

    Updating Carzone

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, TemplateView
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+    
+# def home(request):
+#     return render(request, 'pages/home.html')
+
+class About(TemplateView):
+    template_name = 'pages/about.html'
+
+class Services(TemplateView):
+    template_name = 'pages/services.html'
+
+class Contact(TemplateView):
+    template_name = 'pages/contact.html'
