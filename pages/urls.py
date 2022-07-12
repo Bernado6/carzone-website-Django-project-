84a44b2ce0952c9f8ecb069e5443c8fcee111742
Bernardo6 <bernardo98@students.uonbi.ac,ke>
2022-07-12 13:40:48 +0300

    Completed Teams Model

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', views.Home.as_view(), name = 'home'),
     path('about', views.About.as_view(), name = 'about'),
     path('services', views.Services.as_view(), name = 'services'),
     path('contact', views.Contact.as_view(), name = 'contact'),
--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, TemplateView
-
+from . models import Team
 # Create your views here.
-def home(request):
-    return render(request, 'pages/home.html')
-    
 # def home(request):
-#     return render(request, 'pages/home.html')
+#     teams = Team.objects.all()
+#     data = {
+#         'teams':teams,
+#     }
+#     return render(request, 'pages/home.html',data)
+
+
+class Home(TemplateView):
+    template_name = 'pages/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['teams'] = Team.objects.all().order_by('-id')
+        return context
+
 
 class About(TemplateView):
     template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['teams'] = Team.objects.all().order_by('-id')
+
+        return context
 
 class Services(TemplateView):
     template_name = 'pages/services.html'
